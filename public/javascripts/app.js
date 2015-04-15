df9e57dd8ba59306d5b7c0e9dfab0e35a9b05817
roberttayco <rob@roberttayco.com>
2015-04-15 05:58:04 +0000

    slider updates music location on change

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -264,11 +264,11 @@ var albumPicasso = {
    albumArtUrl: '/images/album-placeholder.png',
 
    songs: [
-       { name: 'Blue', length: '4:26', audioUrl: '/music/placeholders/blue' },
-       { name: 'Green', length: '3:14', audioUrl: '/music/placeholders/green' },
-       { name: 'Red', length: '5:01', audioUrl: '/music/placeholders/red' },
-       { name: 'Pink', length: '3:21', audioUrl: '/music/placeholders/pink' },
-       { name: 'Magenta', length: '2:15', audioUrl: '/music/placeholders/magenta' }
+       { name: 'Blue', length: '163.38', audioUrl: '/music/placeholders/blue' },
+       { name: 'Green', length: '105.66', audioUrl: '/music/placeholders/green' },
+       { name: 'Red', length: '270.14', audioUrl: '/music/placeholders/red' },
+       { name: 'Pink', length: '154.81', audioUrl: '/music/placeholders/pink' },
+       { name: 'Magenta', length: '375.92', audioUrl: '/music/placeholders/magenta' }
      ]
  };
 
@@ -406,6 +406,11 @@ blocJams.service('SongPlayer', function() {
             var song = this.currentAlbum.songs[currentTrackIndex];
             this.setSong(this.currentAlbum, song);
         },
+        seek: function(time) {
+            if (currentSoundFile) {
+                currentSoundFile.setTime(time);
+            }
+        },
         setSong: function(album, song) {
             if (currentSoundFile) {
                 currentSoundFile.stop();
@@ -422,47 +427,85 @@ blocJams.service('SongPlayer', function() {
     };
 });
 
-blocJams.directive('slider', function() {
-
-    var updateSeekPercentage = function($seekBar, event) {
-        var barWidth = $seekBar.width();
-        var offsetX = event.pageX - $seekBar.offset().left;
-
-        var offsetXPercent = (offsetX  / barWidth) * 100;
+blocJams.directive('slider', ['$document', function($document) {
+    var calculateSliderPercentFromMouseEvent = function($slider, event) {
+        var offsetX = event.pageX - $slider.offset().left;
+        var sliderWidth = $slider.width();
+        var offsetXPercent = (offsetX / sliderWidth);
         offsetXPercent = Math.max(0, offsetXPercent);
-        offsetXPercent = Math.min(100, offsetXPercent);
+        offsetXPercent = Math.min(1, offsetXPercent);
+        return offsetXPercent;
+    }
 
-        var percentageString = offsetXPercent + '%';
-        $seekBar.find('.fill').width(percentageString);
-        $seekBar.find('.thumb').css({left: percentageString});
-    };
+    var numberFromValue = function(value, defaultValue) {
+        if (typeof value === 'number') {
+            return value;
+        }
+        if (typeof value === 'undefined') {
+            return defaultValue;
+        }
+        if (typeof value === 'string') {
+            return Number(value);
+        }
+    }
 
     return {
         templateUrl: '/templates/directives/slider.html',
-        replace: true,
-        restrict: 'E',
+        replace:     true,
+        restrict:    'E',
+        scope:       {
+            onChange: '&'
+        },
         link: function(scope, element, attributes) {
+            scope.value = 0;
+            scope.max   = 100;
             var $seekBar = $(element);
 
-            $seekBar.click(function(event) {
-                updateSeekPercentage($seekBar, event);
+            attributes.$observe('value', function(newValue) {
+                scope.value = numberFromValue(newValue, 0);
+            });
+            attributes.$observe('max', function(newValue) {
+                scope.max = numberFromValue(newValue, 100) || 100;
             });
 
-            $seekBar.find('.thumb').mousedown(function(event) {
-                $seekBar.addClass('no-animate');
-                $(document).bind('mousemove.thumb', function(event) {
-                    updateSeekPercentage($seekBar, event);
+            var percentString = function() {
+                var value = scope.value || 0;
+                var max = scope.max || 100;
+                percent = value / max * 100;
+                return percent + '%';
+            };
+            scope.fillStyle = function() {
+                return { width: percentString() };
+            };
+            scope.thumbStyle = function() {
+                return { left: percentString() };
+            };
+            scope.onClickSlider = function(event) {
+                var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
+                scope.value = percent * scope.max;
+                notifyCallback(scope.value);
+            };
+            scope.trackThumb = function() {
+                $document.bind('mousemove.thumb', function(event) {
+                    var percent = calculateSliderPercentFromMouseEvent($seekBar, event);
+                    scope.$apply(function() {
+                        scope.value = percent * scope.max;
+                        notifyCallback(scope.value);
+                    });
                 });
-
-                $(document).bind('mouseup.thumb', function() {
-                    $seekBar.removeClass('no-animate');
-                    $(document).unbind('mousemove.thumb');
-                    $(document).unbind('mouseup.thumb');
+                $document.bind('mouseup.thumb', function() {
+                    $document.unbind('mousemove.thumb');
+                    $document.unbind('mouseup.thumb');
                 });
-            });
+            };
+            var notifyCallback = function(newValue) {
+                if (typeof scope.onChange === 'function') {
+                    scope.onChange({value: newValue});
+                }
+            };
         }
     };
-});
+}]);
 });
 
 ;require.register("scripts/collection", function(exports, require, module) {
